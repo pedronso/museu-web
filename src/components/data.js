119dc1a6ea54af60860a426c9e7e69493353ab5b
pedronso <pedrocaetanoaraujopc@gmail.com>
2023-01-19 13:02:16 -0300

    new description

--- src/components/data.js
+++ src/components/data.js
@@ -5,13 +5,13 @@ export const jsonMock = {
       name:
         "Pistola Luger",
       description:
-          "A pistola Luger P08 é uma antiga pistola semiautomática fabricada na Alemanha, entre os anos de 1900 e 1941. Foi considerada como o maior souvenir da Segunda Guerra Mundial. Esta pistola foi adotada pelo exército alemão em 1908, razão do nome P08.",
+          "A pistola Luger P08 é uma antiga pistola semiautomática fabricada na Alemanha, entre os anos de 1900 e 1941. Foi considerada como o maior souvenir da Segunda Guerra Mundial. Esta pistola foi adotada pelo exército alemão em 1908, razão do nome P08. De calibre 9 mm – parabelum – cano de 4 polegadas. Uma arma destas foi encontrada com Lampião, após sua morte, em Angico.",
       poster:
         "./models/modelo1.png",
     },
     {
         "name": "Carabina Winchester",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "description": "O modelo 1873 era conhecido popularmente como Rifle “Papo Amarelo” devido a uma peça de latão exposta na parte inferior da caixa da culatra (parte posterior do mecanismo de uma Arma de Fogo, onde se aloja o mecanismo de disparo, localizada junto à coronha). Foi vastamente usado no momento inicial do cangaço, perdurando até 1926.",
         "poster": "./models/modelo15.png",
         "linkGLB": "./models/modelo15.glb"
     }
@@ -22,7 +22,7 @@ export const jsonMock = {
       name:
         "Lampião",
       description:
-        "Virgulino Ferreira da Silva, vulgo Lampião, o Rei do Cangaço, foi um cangaceiro brasileiro que atuou na região do sertão nordestino do Brasil.",
+        "Virgolino Ferreira da Silva, vulgo Lampião,  nascido em Vila Bela, atual Serra Talhada, Sertão do Pajeú, que entra para a história com o curioso apelido de Lampião.  Motivado por vingança, mediante a desorganização social dos sertões do Nordeste, tornou-se cangaceiro.",
       poster:
         "./models/modelo2.png",
     },
@@ -39,44 +39,44 @@ export const jsonMock = {
     ],
     Acessórios:[{
         "name": "Cabaça",
-        "description": "Usada para armazenar água, essencial para a sobrevivência no ambiente seco do sertão nordestino.",
+        "description": "Usada para armazenar água, essencial para a sobrevivência no ambiente seco do sertão nordestino. Também utilizada para guardar aguardente.",
         "poster": "./models/modelo11.jpg",
         "linkGLB": "./models/modelo11.glb"
     },
     {
         "name": "Baiaca",
-        "description": "Usado tanto por cangaceiros quanto pela volante, no canudo (ou tubo), eram carregados os objetos preciosos, que não podiam ser expostos ao suor ou chuva, como cartas, dinheiro, ou cigarros.",
+        "description": "Usado tanto por cangaceiros quanto pela volante, na baiaca eram carregados os objetos preciosos, que não podiam ser expostos ao suor ou chuva, como cartas, dinheiro, ou cigarros.",
         "poster": "./models/modelo12.jpg",
         "linkGLB": "./models/modelo12.glb"
     },
     {
         "name": "Cartucheira",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "description": "As cartucheiras eram confeccionadas em couro, para guardar munição. Usavam uma ou duas na cintura e outra, em formato de X, cruzada no tórax, por cima das roupas, estrategicamente posicionadas.",
         "poster": "./models/modelo14.png",
         "linkGLB": "./models/modelo14.glb"
     },
     {
         "name": "Óculos de Lampião",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "description": "Lampião, desde jovem, apresentava problemas de visão. Percebe-se em várias fotografias que seu olho esquerdo tem o globo embranquecido e a pálpebra mais baixa. Entretanto, em 1925 ele teve seu olho esquerdo arranhado por um garrancho de jurema. Após alguns tratamentos, com leves melhoras, Lampião passa a usar óculos escuros, por que a luminosidade do sol provocava incômodos.  Porém, ele só se deixou fotografar com óculos claro, com aros de ouro, por questão de vaidade.",
         "poster": "./models/modelo16.png",
         "linkGLB": "./models/modelo16.glb"
     }
   ],
     Roupas:[{
         "name": "Chapéu de couro",
-        "description": "O chapéu serviu de suporte de arte, já que eram colocados alguns enfeites,e também de alerta, pois nenhum cangaceiro poderia correr o risco de ser surpreendido em uma emboscada, por isso não usavam com a aba abaixada escondendo os olhos.",
+        "description": "O chapéu serviu de suporte de arte, já que eram colocados alguns enfeites, e também de alerta, pois nenhum cangaceiro poderia correr o risco de ser surpreendido em uma emboscada, por isso não usavam com a aba abaixada escondendo os olhos.",
         "poster": "./models/modelo13.png",
         "linkGLB": "./models/modelo13.glb"
     },
     {
-        "name": "Calçados de couro",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "name": "Sandálias de couro",
+        "description": "Podemos dizer que as sandálias dos cangaceiros eram as chamadas alpercatas, que, mediante as amplas distâncias que eram percorridas, a pé, as alpercatas eram confeccionadas em couro de boi, com um solado resistente para longas caminhadas e enfrentamento do solo áspero e dos espinhos da vegetação típica da caatinga. Em cada região que andava com seu bando, Lampião já tinha em determinados locais, remendões para cuidar de seus calçados e do respectivo grupo.",
         "poster": "./models/modelo17.png",
         "linkGLB": "./models/modelo17.glb"
     },
     {
         "name": "Roupa de cangaceiro",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "description": "O conjunto de roupas e adereços usados pelos cangaceiros – incluindo as cartucheiras e munição, cabaças e cantis, embornais e alguma peça de roupas, bugigangas e alguns víveres, passava de trinta quilos - quase dez a mais do que o recomendado pelo Exército para soldados dessas regiões. Após a colocação das vestimentas e apetrechos, os cangaceiros ainda colocavam por cima de tudo uma cartucheira como se fosse para comprimir tudo ao corpo, para evitar que fosse chacoalhado em facilitasse em correria ou rolar ao chão. ",
         "poster": "./models/modelo18.png",
         "linkGLB": "./models/modelo18.glb"
     }
