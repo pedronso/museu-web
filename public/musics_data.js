e84c45c5d1bb6a6373191ec99dc4f8817b615f7c
pedronso <pedrocaetanoaraujopc@gmail.com>
2023-03-24 07:05:42 -0300

    music player on video section

---
+++ public/musics_data.js
@@ -0,0 +1,17 @@
+export default [
+    {
+      title:"Moonlight Sonata 1rd Movement",
+      artist: "Ludwig van Beethoven",
+      file: "Beethoven - Moonlight Sonata 1st Movement.mp3",
+    },
+    {
+      title:"Moonlight Sonata 2rd Movement",
+      artist: "Ludwig van Beethoven",
+      file: "Beethoven - Moonlight Sonata 2st Movement.mp3",
+    },
+    {
+      title:"Moonlight Sonata 3rd Movement",
+      artist: "Ludwig van Beethoven",
+      file: "Beethoven - Moonlight Sonata 3st Movement.mp3",
+    },
+  ];
---
+++ public/player.js
@@ -0,0 +1,80 @@
+import audios from "./musics_data.js";
+import { path, secondsToMinutes } from "./utils.js";
+import elements from "./playerElements.js";
+
+export default {
+  audioData: audios,
+  currentAudio: {},
+  currentPlaying: 0,
+  isPlaying: false,
+  start() {
+    elements.get.call(this);
+    this.update();
+    this.volumeControl.value = 100;
+  },
+  play() {
+    this.isPlaying = true;
+    this.audio.play();
+    this.playPause.className = "fa fa-pause";
+  },
+  pause() {
+    this.isPlaying = false;
+    this.audio.pause();
+    this.playPause.className = "fa fa-play";
+
+  },
+  togglePlayPause() {
+    if (this.isPlaying) {
+      this.pause();
+    } else {
+      this.play();
+    }
+  },
+  next() {
+    ++this.currentPlaying;
+
+    if (this.currentPlaying === this.audioData.length) {
+      this.currentPlaying = 0;
+    }
+    this.pause();
+    this.update();
+    this.play();
+  },
+  back() {
+    --this.currentPlaying;
+
+    if (this.currentPlaying === -1) {
+      this.currentPlaying = this.audioData.length - 1;
+    }
+
+    this.pause();
+    this.update();
+    this.play();
+  },
+  toggleMute() {
+    this.audio.muted = !this.audio.muted;
+    this.volume_icon.className = this.audio.muted ? "fa fa-volume-off" : "fa fa-volume-up";
+  },
+  setVolume(value) {
+    this.audio.volume = value / 100;
+  },
+  setSeekbar(value) {
+    this.audio.currentTime = value;
+  },
+  timeUpdate() {
+    this.currentDuration.innerText = secondsToMinutes(this.audio.currentTime);
+    this.seekbar.value = this.audio.currentTime;
+  },
+  update() {
+    this.currentAudio = this.audioData[this.currentPlaying];
+
+    this.title.innerHTML = this.currentAudio.title;
+    this.artist.innerHTML = this.currentAudio.artist;
+
+    elements.createAudioElement.call(this, path(this.currentAudio.file));
+
+    this.audio.onloadeddata = () => {
+      elements.actions.call(this);
+    };
+  },
+};
